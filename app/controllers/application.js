868ca1a21e369f3c9affbe41aef36251c444179e
Blake Erickson <o.blakeerickson@gmail.com>
2014-11-07 17:05:39 -0700

    add check for clear and check for calc

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,55 +5,71 @@ export default Ember.ArrayController.extend({
   operator: '',
   operand_1: '',
   operand_2: '',
+  after_calc: 0,
 
   actions: {
     zero: function() {
+      this.checkForClear();
       this.append('0');
     },
     one: function() {
+      this.checkForClear();
       this.append('1');
     },
     two: function() {
+      this.checkForClear();
       this.append('2');
     },
     three: function() {
+      this.checkForClear();
       this.append('3');
     },
     four: function() {
+      this.checkForClear();
       this.append('4');
     },
     five: function() {
+      this.checkForClear();
       this.append('5');
     },
     six: function() {
+      this.checkForClear();
       this.append('6');
     },
     seven: function() {
+      this.checkForClear();
       this.append('7');
     },
     eight: function() {
+      this.checkForClear();
       this.append('8');
     },
     nine: function() {
+      this.checkForClear();
       this.append('9');
     },
     decimal: function() {
+      this.checkForClear();
       this.append('.');
     },
     plus: function() {
+      this.checkForCalc();
       this.set('operator', '+');
     },
     minus: function() {
+      this.checkForCalc();
       this.set('operator', '-');
     },
     multiply: function() {
+      this.checkForCalc();
       this.set('operator', '*');
     },
     divide: function() {
+      this.checkForCalc();
       this.set('operator', '/');
     },
     equals: function() {
-      this.calc();
+      this.checkForCalc();
     },
     clear: function() {
       this.clear();
@@ -76,11 +92,28 @@ export default Ember.ArrayController.extend({
     }
   },
 
+  checkForCalc: function() {
+    if (this.get('operand_1') !== '' &&
+        this.get('operand_2') !== '' &&
+        this.get('operator') !== '') {
+      this.calc();
+    }
+  },
+
+  checkForClear: function() {
+    if (this.get('operand_1') !== '' &&
+        this.get('operator') === '' &&
+        parseInt(this.get('after_calc')) > 0) {
+      this.clear();
+    }
+  },
+
   clear: function() {
     this.set('operand_1', '');
     this.set('aperand_2', '');
     this.set('operator', '');
     this.set('myDisplay', '');
+    this.set('after_calc', 0);
   },
 
   calc: function() {
@@ -108,5 +141,6 @@ export default Ember.ArrayController.extend({
     this.set('operand_1', result);
     this.set('operand_2', '');
     this.set('operator', '');
+    this.set('after_calc', this.get('after_calc') + 1);
   }
 });
