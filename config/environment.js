409f5f5074c7928a790a0f5d80a054bba41c3a51
Blake Erickson <o.blakeerickson@gmail.com>
2014-11-07 17:13:13 -0700

    updated baseurl

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'embercalc',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/embercalc',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
